3ba151b7b6786e1af0b364f58814195355b56731
Jay A. Patel <jay@patel.org.in>
2015-01-20 11:29:51 -0800

    Generating new project from lein template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to coin-change
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/coin_change/core.clj
@@ -0,0 +1,7 @@
+(ns coin-change.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
