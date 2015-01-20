b7160211190fe15e0f016c32c93330dd6c3773a3
Jay A. Patel <jay@patel.org.in>
2015-01-20 15:43:16 -0800

    Clean up test code

--- test/making_change/core_test.clj
+++ test/making_change/core_test.clj
@@ -1,6 +1,5 @@
 (ns making-change.core-test
-  (:require [clojure.test :refer :all]
-            [clojure.test.check.clojure-test :refer [defspec]]
+  (:require [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [making-change.core :refer :all]))
@@ -15,28 +14,27 @@
   pick-amount-from-coinset
   (gen/bind mk-coinset #(gen/tuple (gen/return %) (gen/elements %))))
 
-;; Change must add up (or function is committing fraud! *gasp!*)
+;; Change must add up -- or else, *gasp!* -- the function is committing fraud!
 (defspec correct-change
          (prop/for-all [coinset mk-coinset
                         amount gen/s-pos-int]
                        (let [change (make-change coinset amount)]
                          (= amount (apply + change)))))
 
-;; If the amount is one of the coins of the coinset, that should be the change
+;; If the amount is one of the coins of the coinset, that must be the change
 (defspec smallest-change
          (prop/for-all [[coinset amount] pick-amount-from-coinset]
                        (let [change (make-change coinset amount)]
                          (= [amount] change))))
 
-;; The number of coins to change double-amount should be at most twice the
+;; The number of coins to change double the amount must be at most twice the
 ;; number of coins to change amount
 (defspec double-amount-change
          (prop/for-all [coinset mk-coinset
                         amount gen/s-pos-int]
                        ;; NOTE: changing dp-change to greedy-change will make
                        ;; this test spec fail
-                       (let [mk-change (partial make-change dp-change coinset)
-                             change (mk-change amount)
-                             double-amount (* amount 2)
-                             double-change #spy/d (mk-change double-amount)]
+                       (let [f (partial make-change dp-change coinset)
+                             change (f amount)
+                             double-change (f (* amount 2))]
                          (<= (count double-change) (* (count change) 2)))))
